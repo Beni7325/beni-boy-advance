353f4a46383c3e545213f88ee97b2f457acfc4fd
Beni7325 <alejandro1benimeli@gmail.com>
2023-01-10 23:22:52 +0100

    Added more checks to cartridge constructor

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -1,8 +1,6 @@
 use std::path::Path;
 
 
-const EXTERNAL_RAM_SIZES: [usize; 4] = [0, 2048, 8192, 32768];
-
 pub struct Cartridge {
     rom: Box<[u8]>,
     rom_size: usize,
@@ -33,11 +31,15 @@ impl Cartridge {
             return Err(CartridgeError::InvalidRomError)
         }
 
-        if rom[0x149] >= 4 {
-            return Err(CartridgeError::InvalidRomError)
+        match get_rom_size(rom[0x148]) {
+            Some(size) if size == rom_size => {},
+            _ => return Err(CartridgeError::InvalidRomError)
         }
 
-        let external_ram_size = EXTERNAL_RAM_SIZES[rom[0x149] as usize];
+        let external_ram_size = match get_ext_ram_size(rom[0x149]) {
+            Some(size) => size,
+            None => return Err(CartridgeError::InvalidRomError)
+        };
 
         Ok(Cartridge { 
             rom: rom.into_boxed_slice(),
@@ -47,6 +49,33 @@ impl Cartridge {
             external_ram_bank: 0x0000
         })
         
+    }
+
+}
+
+fn get_ext_ram_size(byte: u8) -> Option<usize> {
+    match byte {
+        0x00 => Some(0x00000),  // No RAM
+        0x02 => Some(0x02000),  // 1 Bank (8 KiB)
+        0x03 => Some(0x08000),  // 4 Banks (32 KiB)
+        0x04 => Some(0x20000),  // 16 Banks (128 KiB)
+        0x05 => Some(0x10000),  // 8 Banks (64 KiB)
+        _ => None
+    }
+}
+
+fn get_rom_size(byte: u8) -> Option<usize> {
+    match byte {
+        0x00 => Some(0x008000),  // 32 KiB
+        0x01 => Some(0x010000),  // 64 KiB
+        0x02 => Some(0x020000),  // 128 KiB
+        0x03 => Some(0x040000),  // 256 KiB
+        0x04 => Some(0x080000),  // 512 KiB
+        0x05 => Some(0x100000),  // 1 MiB
+        0x06 => Some(0x200000),  // 2 MiB
+        0x07 => Some(0x400000),  // 4 MiB
+        0x08 => Some(0x800000),  // 8 MiB
+        _ => None
     }
 
 }
