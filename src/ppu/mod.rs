880db29c78d6103c984534f83510c5fa2055d647
Beni7325 <alejandro1benimeli@gmail.com>
2023-02-03 17:07:17 +0100

    PPU regs

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,6 +1,26 @@
+enum PpuState {
+    Glitched,
+
+}
+
+
 pub struct Ppu {
     vram: Box<[u8; 0x2000]>,
-    oam: Box<[u8; 0x00A0]>
+    oam: Box<[u8; 0x00A0]>,
+
+    wx: u8,
+    wy: u8,
+    ly: u8,
+    lyc: u8,
+    scx: u8,
+    scy: u8,
+
+    bgp: u8,
+    obp0: u8,
+    obp1: u8,
+
+    lcdc: u8,
+    stat: u8
 }
 
 impl Ppu {
@@ -8,7 +28,18 @@ impl Ppu {
     pub fn new() -> Ppu {
         Ppu {
             vram: vec![0; 0x2000].into_boxed_slice().try_into().expect("Array size mismatch!"),
-            oam: vec![0; 0x00A0].into_boxed_slice().try_into().expect("Array size mismatch!")
+            oam: vec![0; 0x00A0].into_boxed_slice().try_into().expect("Array size mismatch!"),
+            wx: 0x00,
+            wy: 0x00,
+            ly: 0x00,
+            lyc: 0x00,
+            scx: 0x00,
+            scy: 0x00,
+            bgp: 0xFC,
+            obp0: 0xFF, // Revise
+            obp1: 0xFF, // Revise
+            lcdc: 0x91,
+            stat: 0x85
         }
     }
 
@@ -26,6 +57,16 @@ impl Ppu {
 
     pub fn write_oam(&mut self, addr: u16, data: u8) {
         self.oam[addr as usize] = data;
+    }
+
+    pub fn tick(&mut self, m_cycles: u64, interrupt_flag: &mut u8) {
+
+        for _ in 0..m_cycles {
+
+
+
+        }
+
     }
 
 }
